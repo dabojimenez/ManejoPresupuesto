790e75c7f586c6019663c6942287ffd4ce841486
David <dabojimenez16@hotmail.com>
2023-04-18 20:56:42 -0500

    Se agrega en la vista Crear.cshtml, la libreria de jqueryValidation, para validar desde la aprte del front-end los datos, sin que llegue al metodo post de nuestro controlador, si en caso no se registra de forma correcta la informacion, usamos la vista parcial, _validationScriptsPartial.cshtml

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -13,16 +13,16 @@ namespace ManejoPresupuesto.Models
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
 
-        /*Pruebas de otras validaciones por defecto*/
-        [Required(ErrorMessage = "El campo {0} es requerido")]
-        [EmailAddress(ErrorMessage = "El campo debe ser un correo electronico valido")]
-        public string? Email { get; set; }
-        [Range(minimum:18, maximum: 130, ErrorMessage = "Valor entre [{1} - {2}]")]
-        public int Edad { get; set; }
-        [Url(ErrorMessage = "El campo {0} debe ser una URL valida")]
-        public string? URL { get; set; }
-        [CreditCard(ErrorMessage = "La tarjeta de credito no es valida")]
-        [Display(Name = "Tarjeta de Credito")]
-        public string? TarjetaDeCredito { get; set; }
+        ///*Pruebas de otras validaciones por defecto*/
+        //[Required(ErrorMessage = "El campo {0} es requerido")]
+        //[EmailAddress(ErrorMessage = "El campo debe ser un correo electronico valido")]
+        //public string? Email { get; set; }
+        //[Range(minimum:18, maximum: 130, ErrorMessage = "Valor entre [{1} - {2}]")]
+        //public int Edad { get; set; }
+        //[Url(ErrorMessage = "El campo {0} debe ser una URL valida")]
+        //public string? URL { get; set; }
+        //[CreditCard(ErrorMessage = "La tarjeta de credito no es valida")]
+        //[Display(Name = "Tarjeta de Credito")]
+        //public string? TarjetaDeCredito { get; set; }
     }
 }
