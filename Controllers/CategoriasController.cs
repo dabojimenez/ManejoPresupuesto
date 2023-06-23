69538e26aacdc74a3877e30241c277826d91970a
David Jimenez <dabojimenez16@gmail.com>
2023-06-22 20:32:06 -0500

    Se agrega los meotdfos de actualizar y eliminar de la clase categorias

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -15,9 +15,12 @@ namespace ManejoPresupuesto.Controllers
             this.repositorioCategorias = repositorioCategorias;
             this.servicioUsuarios = servicioUsuarios;
         }
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var usuarioid = servicioUsuarios.ObtenerUsuarioId();
+            var categorias = await repositorioCategorias.Obtener(usuarioid);
+            return View(categorias);
         }
 
         [HttpGet]
@@ -37,7 +40,61 @@ namespace ManejoPresupuesto.Controllers
             categoria.UsuarioId = usuarioId;
             await repositorioCategorias.Crear(categoria);
             return RedirectToAction("Index");
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var categoria = await repositorioCategorias.ObtenerPorId(id, usuarioId);
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(Categoria categoriaEditar)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(categoriaEditar);
+            }
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var categoria = await repositorioCategorias.ObtenerPorId(categoriaEditar.Id, usuarioId);
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            categoriaEditar.UsuarioId = usuarioId;
+            await repositorioCategorias.Actualizar(categoriaEditar);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Borrar(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var categoria = await repositorioCategorias.ObtenerPorId(id, usuarioId);
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BorrarCategoria(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var categoria = await repositorioCategorias.ObtenerPorId(id, usuarioId);
+            if (categoria is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            await repositorioCategorias.Borrar(id);
+            return RedirectToAction("Index");
         }
     }
 }
--- Servicio/RepositorioCategorias.cs
+++ Servicio/RepositorioCategorias.cs
@@ -6,7 +6,11 @@ namespace ManejoPresupuesto.Servicio
 {
     public interface IRepositorioCategorias
     {
+        Task Actualizar(Categoria categoria);
+        Task Borrar(int id);
         Task Crear(Categoria categoria);
+        Task<IEnumerable<Categoria>> Obtener(int usuarioId);
+        Task<Categoria> ObtenerPorId(int id, int usuarioid);
     }
     public class RepositorioCategorias: IRepositorioCategorias
     {
@@ -24,6 +28,38 @@ namespace ManejoPresupuesto.Servicio
                         VALUES (@Nombre, @TIPOOPERACIONID, @USUARIOID);
                         SELECT SCOPE_IDENTITY();", categoria);
             categoria.Id = id;
+        }
+
+        public async Task<IEnumerable<Categoria>> Obtener(int usuarioId)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            return await connection.QueryAsync<Categoria>(@"
+                        SELECT * FROM CATEGORIAS WHERE USUARIOID = @USUARIOID",
+                        new { usuarioId });
+        }
+
+        public async Task<Categoria> ObtenerPorId(int id, int usuarioid)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            return await connection.QueryFirstOrDefaultAsync<Categoria>(@"
+                        SELECT * FROM CATEGORIAS WHERE Id = @Id AND UsuarioId = @UsuarioId", 
+                        new {id, usuarioid});
+        }
+
+        public async Task Actualizar(Categoria categoria)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            await connection.ExecuteAsync(@"
+                        UPDATE CATEGORIAS SET NOMBRE = @NOMBRE, TIPOOPERACIONID = @TIPOOPERACIONID
+                        WHERE ID = @ID",
+                        categoria);
+        }
+
+        public async Task Borrar(int id)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            await connection.ExecuteAsync(@"DELETE Categorias WHERE Id = @Id",
+                new {id});
         }
     }
 }
